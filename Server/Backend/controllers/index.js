ba8e2aa5534eeaad7844f4f724f956409d11c8ed
Aryanop0914 <21cs038@charusat.edu.in>
2023-08-27 09:10:37 +0530

    w

--- Server/Backend/controllers/index.js
+++ Server/Backend/controllers/index.js
@@ -1,6 +1,6 @@
 const registerController = require("./auth/registerController.js");
 const loginController = require("./auth/loginController.js");
-const eventController = require("./Events/eventController.js");
+const eventController = require("./events/eventController.js");
 module.exports = {
   registerController,
   loginController,
